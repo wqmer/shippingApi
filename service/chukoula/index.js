d3f0307a2bb1d7a0b782675a895db6f8b52b1a98
kimmy <wqmer11532@gmail.com>
2020-02-09 17:15:26 -0800

    update deftship

--- service/chukoula/index.js
+++ service/chukoula/index.js
@@ -1,30 +1,27 @@
 const request = require('request');
 
 
-const createOrder = (order , callback) =>{
-    request({
-        method: 'POST',
-        headers: { "content-type": "application/json" },
-        url:    'http://test.chukou.la/action/interface.api.php',
-        body:   JSON.stringify(order)
-      }, function(error, response, body){
-        //  let orderId = order.order.referenceNumber
-        // console.log(response.body)
-      try {  
-        s = response.body.substr(1);
-             // let myresponse = response.body
-             callback(null, JSON.parse(s));
-      } catch (error) {
-             callback(null, {  "code": 505 , "message": "server disconnect" });
-      } 
-        // console.log(typeof(response.body))
-        // let myresponse = JSON.parse(response.body)
-   
-      }); 
+const createOrder = (order, callback) => {
+  request({
+    method: 'POST',
+    headers: { "content-type": "application/json" },
+    url: 'http://test.chukou.la/action/interface.api.php',
+    body: JSON.stringify(order)
+  }, function (error, response, body) {
+    //  let orderId = order.order.referenceNumber
+    // console.log(response.body)
+    try {
+      s = response.body.substr(1);
+      // let myresponse = response.body
+      callback(null, JSON.parse(s));
+    } catch (error) {
+      callback(null, { "code": 505, "message": "server disconnect" });
+    }
+  });
 }
 
 
 
-module.exports = { 
-       createOrder 
+module.exports = {
+  createOrder
 }
