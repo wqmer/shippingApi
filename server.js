74590de542caeeba467b0a42b2853a87824bdff1
kimmy <wqmer11532@gmail.com>
2020-03-11 22:50:09 -0700

    pm2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var ip = require('ip');
 require('body-parser-xml')(bodyParser);
 
 var app = express();
-var port = process.env.PORT || 8080 ;
+var port = process.env.PORT || 5000 ;
 
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
