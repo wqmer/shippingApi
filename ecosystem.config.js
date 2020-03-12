bd5b6d030da0874f9252c40e4b57eec8ed237df5
kimmy <wqmer11532@gmail.com>
2020-03-11 22:24:32 -0700

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,10 @@
 module.exports = {
   apps: [{
     name: 'Huodaios_hk',
-    script: 'server.js',
+    script: 'server.js',}
 
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+],
 
   deploy: {
     production: {
