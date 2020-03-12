56fe32e342f2d393aacede24109f2feb75aff2d6
kimmy <wqmer11532@gmail.com>
2020-03-11 22:02:24 -0700

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       host: ['hk.huodaios.com'],                                // 服务器地址
       ref: 'huodaiOs/master',
       repo: 'git@github.com:wqmer/huodaiOS_hongKong_middle.git',            // github上的项目地址
-      path: '/home',                                                //  服务器上放项目的目录
+      path: '/home/huodai_os_hk',                                                //  服务器上放项目的目录
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
